e27a5c967b48714944c09f0429d222dbd590287f
Nolo Marsh <nolo.marsh@gmail.com>
2021-12-21 14:14:12 -0500

    switching url to remote-hosted api

--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -1,6 +1,2 @@
-export let apiUrl = 'http://localhost:3001/'
-
-if(process.env.API_URL) {
-  apiUrl = process.env.API_URL
-  // 'https://wut2play-api.herokuapp.com/'
-}
+export let apiUrl = 'https://wut2play-api.herokuapp.com/'
+// 'http://localhost:3001/'
